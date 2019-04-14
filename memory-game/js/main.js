f1417cba8c786d8786aa0c4de86411694ce84d6b
Nader Althubaity <na511dr@hotmail.com>
2019-04-14 17:43:47 +0300

    Created the js file to get into game logic

---
+++ memory-game/js/main.js
@@ -0,0 +1,12 @@
+var cardOne, cardTwo, cardThree, cardFour;
+cardOne = "queen";
+cardTwo = "queen";
+cardThree = "king";
+cardFour = "king";
+
+console.log("User flipped " + cardTwo);
+console.log("User flipped " + cardThree);
+
+
+
+
