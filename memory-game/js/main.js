cc8a3e88c904365a450790bfc261506afc7c132f
Nader Althubaity <na511dr@hotmail.com>
2019-04-16 16:33:34 +0300

    Organizing code by using functions flipCards(), checkCards()

--- memory-game/js/main.js
+++ memory-game/js/main.js
@@ -1,20 +1,42 @@
 
 var cards = ["queen" , "queen" , "king" , "king"];
 var cardsInPlay = [];
-var cardOne = cards[1];
-cardsInPlay.push(cardOne);
-console.log("User flipped " + cardOne);
-var cardTwo = cards[0];
-cardsInPlay.push(cardTwo);
-console.log("User flipped " + cardTwo);
 
-if (cardsInPlay.length === 2) {
+var checkCards = function () {
 
-    if (cardsInPlay[0] === cardsInPlay[1]) {
-        alert("You found a darn match");
+    if (cardsInPlay.length === 2 && cardsInPlay[0] === cardsInPlay[1]) {
+
+        alert("God damn it, you made it")
+    }
+
+    else if (cardsInPlay.length === 2) {
+        alert(" No Match, sorry");
     }
     else {
-        alert("Sorry Try Again");
+        alert("No Enough Plays");
     }
- }
+
+
+};
+var flipCard = function (cardId) {
+
+   /* These lines of code have been moved to checkCards()
+
+    var cardOne = cards[1];
+    cardsInPlay.push(cardOne);
+    console.log("User flipped " + cardOne);
+    var cardTwo = cards[0];
+    cardsInPlay.push(cardTwo);
+    console.log("User flipped " + cardTwo);
+*/
+   var cardOne = cards[cardId];
+    cardsInPlay.push(cardOne);
+    console.log("you flipped: " + cardOne);
+
+};
+        flipCard(1);
+        flipCard(0);
+
+checkCards();
+
 
