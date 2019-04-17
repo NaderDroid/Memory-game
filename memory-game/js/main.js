215ff15fb5a22d4438826a046ad6edc2b34a43bf
Nader Althubaity <na511dr@hotmail.com>
2019-04-17 03:06:27 +0300

    Updating 'cards' array to contain objects instead of regular elements

--- memory-game/js/main.js
+++ memory-game/js/main.js
@@ -1,22 +1,42 @@
 
-var cards = ["queen" , "queen" , "king" , "king"];
+var cards = [
+    {
+        rank : "queen",
+        suit : "hearts",
+        cardImage : "images/queen-of-hearts.png"
+    },
+    {
+        rank : "queen",
+        suit : "diamonds",
+        cardImage : "images/queen-of-diamonds.png"
+    },
+    {
+        rank : "king",
+        suit : "hearts",
+        cardImage : "images/king-of-hearts.png"
+    },
+    {
+        rank : "king",
+        suit : "diamonds",
+        cardImage : "images/king-of-diamonds.png"
+    }
+];
+
 var cardsInPlay = [];
 
 var checkCards = function () {
 
     if (cardsInPlay.length === 2 && cardsInPlay[0] === cardsInPlay[1]) {
-
         alert("God damn it, you made it")
     }
 
     else if (cardsInPlay.length === 2) {
         alert(" No Match, sorry");
     }
+
     else {
         alert("No Enough Plays");
     }
-
-
 };
 var flipCard = function (cardId) {
 
@@ -29,9 +49,11 @@ var flipCard = function (cardId) {
     cardsInPlay.push(cardTwo);
     console.log("User flipped " + cardTwo);
 */
-   var cardOne = cards[cardId];
-    cardsInPlay.push(cardOne);
-    console.log("you flipped: " + cardOne);
+    cardsInPlay.push(cards[cardId].rank);
+    console.log("you flipped: " + cards[cardId].rank);
+    console.log(cards[cardId].suit);
+    console.log(cards[cardId].cardImage);
+
 
 };
         flipCard(1);
